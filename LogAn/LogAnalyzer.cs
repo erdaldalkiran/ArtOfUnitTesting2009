2f3cf97390735093c37b60df4939152588756674
erdalkiran <erdalkiran@gmail.com>
2014-01-03 19:11:54 +0200

    Chapter 2

---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿namespace LogAn
+{
+    using System;
+    using System.IO;
+
+    public class LogAnalyzer
+    {
+        public bool WasLastFileNameValid { get; set; }
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("No filename provided");
+            }
+            return WasLastFileNameValid = fileName.ToLowerInvariant().EndsWith(".slf");
+        }
+    }
+}
